d4f1d67987f4a1d774c6973c0ab5359c02598661
JDCar1241 <jcardenasv3.a@upao.edu.pe>
2026-06-13 23:23:10 -0500

    feat: Creacion de LibroIterator :)

--- src/biblioteca/iterator/LibroIterator.java
+++ src/biblioteca/iterator/LibroIterator.java
@@ -1,4 +1,31 @@
 package biblioteca.iterator;
 
-public class LibroIterator {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import biblioteca.modelo.Libro;
+
+public class LibroIterator implements Iterator {
+
+
+    private ArrayList<Libro> libros;
+    private int posicion = 0;
+
+    public LibroIterator(ArrayList<Libro> libros) {
+        this.libros = new ArrayList<>(libros);
+
+        // Ordenar por autor
+        Collections.sort(this.libros, Comparator.comparing(Libro::getAutor));
+    }
+
+    @Override
+    public boolean hasNext() {
+        return posicion < libros.size();
+    }
+
+    @Override
+    public Libro next() {
+        return libros.get(posicion++);
+    }
+
 }
