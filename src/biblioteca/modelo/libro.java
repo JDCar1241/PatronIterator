11c2aece21efd361d6dcde56b7e46788f45791d5
JDCar1241 <jcardenasv3.a@upao.edu.pe>
2026-06-13 22:49:38 -0500

    Primer commit

---
+++ src/biblioteca/modelo/libro.java
@@ -0,0 +1,51 @@
+package biblioteca.modelo;
+
+public class Libro {
+
+    private int id;
+    private String titulo;
+    private String autor;
+    private String categoria;
+    private boolean disponible;
+
+    // Constructor
+    public Libro(int id, String titulo, String autor, String categoria, boolean disponible) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.categoria = categoria;
+        this.disponible = disponible;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+                "\nTítulo: " + titulo +
+                "\nAutor: " + autor +
+                "\nCategoría: " + categoria +
+                "\nDisponible: " + (disponible ? "Sí" : "No") +
+                "\n--------------------------------";
+    }
+
+}
